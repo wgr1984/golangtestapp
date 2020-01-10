0d46bd618b386297a76c795e11de613275420823
Wolfgang Reithmeier <w.reithmeier@gmail.com>
2020-01-10 16:28:43 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"sub"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	sub.Hello("Test")
+}
---
+++ sub/submodule.go
@@ -0,0 +1,9 @@
+package sub
+
+import (
+	"fmt"
+)
+
+func Hallo(out string)  {
+	fmt.Printf("Hello %", out)
+}
